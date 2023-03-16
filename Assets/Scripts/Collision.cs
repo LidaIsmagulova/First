c17da690dbd6a2f797c538c99c0643d75019b3d4
Lida <Ismagulovalida@gmail.com>
2023-03-16 18:17:19 +0600

    mywork

---
+++ Assets/Scripts/Collision.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+[RequireComponent (typeof(Rigidbody))]
+public class Collision : MonoBehaviour
+{
+  [SerializeField] private float timeToReloadScene = 2f;
+  [SerializeField] private ParticleSystem crushVFX;
+
+  private void Start()
+  {
+    GetComponent<Rigidbody>().useGravity=false;
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    StartCrushSequence();
+  }
+ 
+  private void StartCrushSequence()
+  {
+    GetComponent<Player>().enabled=false;
+    GetComponent<MeshCollider>().enabled=false;
+    GetComponent<MeshRenderer>().enabled=false;
+    crushVFX.Play();
+    Invoke(nameof(ReloadLevel), timeToReloadScene);
+
+  }
+
+  private void ReloadLevel()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+  }
+}
